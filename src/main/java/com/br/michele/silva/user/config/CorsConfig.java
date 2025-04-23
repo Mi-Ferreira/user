97e50486a9341416c61cf099ea45acbf87dfab02
Elison Nascimento <elisonno997@gmail.com>
2025-04-22 21:44:27 -0300

    Ajustando docker-compose, yml

---
+++ src/main/java/com/br/michele/silva/user/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.br.michele.silva.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // Permitir todas as origens
+        configuration.addAllowedMethod("*"); // Permitir todos os métodos
+        configuration.addAllowedHeader("*"); // Permitir todos os cabeçalhos
+        configuration.setAllowCredentials(true); // Permitir credenciais
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
